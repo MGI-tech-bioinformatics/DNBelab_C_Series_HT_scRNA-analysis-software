a253cae39843f57f5860a72fccef352ac93f3282
liangxinming@genomics.cn <1121913121@qq.com>
2022-06-21 10:16:18 +0800

    INIT

--- example/run.sh
+++
@@ -1,2 +0,0 @@
-export PATH=/Local/path/miniconda3/envs/scRNA/bin:$PATH
-java -jar /Local/path/pipeline/workflows/cromwell-35.jar run -i config.json /Local/path/pipeline/workflows/Droplet_scRNA_V2.3.wdl
